b2e3cbd338f16b2c27b1b42f3bd961e8097e5c79
Honestraj <honestraj.it@gmail.com>
2019-05-23 13:05:26 +0530

    Rspec: Cleanup

---
+++ rspec_correction.rb
@@ -0,0 +1,15 @@
+file = File.read("revremit_correction.txt")
+f = file.split("\n")
+file_name_arr = f.map{|a| a.split(" # ")[0] }
+
+file_name_arr.each do |file_name|
+ file, line_no = file_name.split(":")
+ exact_file = file.split(" .")
+ File.open("#{Rails.root}#{exact_file[1]}", 'r+') do |file|
+  lines_arr = File.readlines("#{Rails.root}#{exact_file[1]}")
+  skip_line = lines_arr[line_no.to_i-1]	
+  p skip_line
+  line_read = File.read("#{Rails.root}#{exact_file[1]}").sub(skip_line,skip_line.sub("it","xit"))
+  file.puts(line_read)
+ end
+end
